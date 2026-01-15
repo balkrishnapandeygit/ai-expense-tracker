f44f5a0cee232a28cc1ba3ef409bf288cc8cc19c
balkrishnapandeygit <balkrishnapandey2005@gmail.com>
2026-01-15 21:35:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,6 @@ console.log("Server file started");
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
@@ -33,7 +32,7 @@ app.use("/api/ai", protect, aiRoutes);
 connectDB();
 
 
-// Test Route
+
 app.get("/", (req, res) => {
   res.send("AI Expense Tracker Backend Running 🚀");
 });
