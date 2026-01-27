3e485e981fe3531d8e96e0fd32096271c751555e
balkrishnapandeygit <balkrishnapandey2005@gmail.com>
2026-01-28 00:15:41 +0530

    Update expenseRoutes.js

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -9,16 +9,15 @@ const {
   deleteExpense,
 } = require("../controllers/expenseController");
 
-// GET all expenses
+
 router.get("/", protect, getExpenses);
 
-// ADD expense
+
 router.post("/", protect, addExpense);
 
-// UPDATE expense
 router.put("/:id", protect, updateExpense);
 
-// DELETE expense
+
 router.delete("/:id", protect, deleteExpense);
 
 module.exports = router;
